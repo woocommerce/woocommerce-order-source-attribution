c69a58e26b3d5c6d863bc78bdd4bac75dda44a87
Jeremy Pry <jeremy.pry@gmail.com>
2022-11-23 18:29:44 -0500

    Create plugin factory

---
+++ src/Internal/PluginFactory.php
@@ -0,0 +1,30 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Grow\OrderAttributePrototype\Internal;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class PluginFactory
+ *
+ * This is responsible for instantiating a Plugin object and returning the same object.
+ *
+ * @since x.x.x
+ */
+final class PluginFactory {
+
+	/**
+	 * Get the instance of the Plugin object.
+	 *
+	 * @return Plugin
+	 */
+	public static function instance(): Plugin {
+		static $plugin = null;
+		if ( null === $plugin ) {
+			$plugin = new Plugin();
+		}
+
+		return $plugin;
+	}
+}

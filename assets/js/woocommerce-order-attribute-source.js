baac0823004001b6905e84cbdc09855c2e229617
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2023-01-20 14:48:28 +0200

    Check for tracking before init.

--- assets/js/woocommerce-order-attribute-source.js
+++ assets/js/woocommerce-order-attribute-source.js
@@ -1,56 +1,68 @@
 ( function ( $, params ) {
 	'use strict';
 
+	window.woocommerce_order_source_attribution = {};
+
 	const prefix = params.prefix;
 	const cookieLifetime = Number(params.lifetime);
 	const sessionLength = Number(params.session);
 
-	/**
-	 * Initialize sourcebuster.js.
-	 */
-	sbjs.init( {
-		lifetime: cookieLifetime,
-		session_length: sessionLength,
-		timezone_offset: '0', // utc
-	} );
+	// init
+	woocommerce_order_source_attribution.initOrderTracking = function() {
 
-	/**
-	 * Set values.
-	 */
-	var setFields = function () {
-
-		if ( sbjs.get ) {
-			$( `input[name="${prefix}type"]` ).val( sbjs.get.current.typ );
-			$( `input[name="${prefix}url"]` ).val( sbjs.get.current_add.rf );
-
-			$( `input[name="${prefix}utm_campaign"]` ).val( sbjs.get.current.cmp );
-			$( `input[name="${prefix}utm_source"]` ).val( sbjs.get.current.src );
-			$( `input[name="${prefix}utm_medium"]` ).val( sbjs.get.current.mdm );
-			$( `input[name="${prefix}utm_content"]` ).val( sbjs.get.current.cnt );
-			$( `input[name="${prefix}utm_id"]` ).val( sbjs.get.current.id );
-			$( `input[name="${prefix}utm_term"]` ).val( sbjs.get.current.trm );
-
-			$( `input[name="${prefix}session_entry"]` ).val( sbjs.get.current_add.ep );
-			$( `input[name="${prefix}session_start_time"]` ).val( sbjs.get.current_add.fd );
-			$( `input[name="${prefix}session_pages"]` ).val( sbjs.get.session.pgs );
-			$( `input[name="${prefix}session_count"]` ).val( sbjs.get.udata.vst );
-
-			$( `input[name="${prefix}user_agent"]` ).val( sbjs.get.udata.uag );
+		if ( typeof window.allowTracking === 'undefined' || window.allowTracking === false ) {
+			return;
 		}
-	};
 
-	/**
-	 * Add source values to checkout.
-	 */
-	$( document.body ).on( 'init_checkout', function () {
-		setFields();
-	} );
+		/**
+		 * Initialize sourcebuster.js.
+		 */
+		sbjs.init( {
+			lifetime: cookieLifetime,
+			session_length: sessionLength,
+			timezone_offset: '0', // utc
+		} );
 
-	/**
-	 * Add source values to register.
-	 */
-	if ( $( '.woocommerce form.register' ).length ) {
-		setFields();
+		/**
+		 * Set values.
+		 */
+		var setFields = function () {
+
+			if ( sbjs.get ) {
+				$( `input[name="${prefix}type"]` ).val( sbjs.get.current.typ );
+				$( `input[name="${prefix}url"]` ).val( sbjs.get.current_add.rf );
+
+				$( `input[name="${prefix}utm_campaign"]` ).val( sbjs.get.current.cmp );
+				$( `input[name="${prefix}utm_source"]` ).val( sbjs.get.current.src );
+				$( `input[name="${prefix}utm_medium"]` ).val( sbjs.get.current.mdm );
+				$( `input[name="${prefix}utm_content"]` ).val( sbjs.get.current.cnt );
+				$( `input[name="${prefix}utm_id"]` ).val( sbjs.get.current.id );
+				$( `input[name="${prefix}utm_term"]` ).val( sbjs.get.current.trm );
+
+				$( `input[name="${prefix}session_entry"]` ).val( sbjs.get.current_add.ep );
+				$( `input[name="${prefix}session_start_time"]` ).val( sbjs.get.current_add.fd );
+				$( `input[name="${prefix}session_pages"]` ).val( sbjs.get.session.pgs );
+				$( `input[name="${prefix}session_count"]` ).val( sbjs.get.udata.vst );
+
+				$( `input[name="${prefix}user_agent"]` ).val( sbjs.get.udata.uag );
+			}
+		};
+
+		/**
+		 * Add source values to checkout.
+		 */
+		$( document.body ).on( 'init_checkout', function () {
+			setFields();
+		} );
+
+		/**
+		 * Add source values to register.
+		 */
+		if ( $( '.woocommerce form.register' ).length ) {
+			setFields();
+		}
 	}
 
+	// Run init.
+	woocommerce_order_source_attribution.initOrderTracking();
 } )( jQuery, window.wc_order_attribute_source_params );

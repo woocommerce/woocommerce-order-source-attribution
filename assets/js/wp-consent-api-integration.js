335ce69ac6dbff74cefdbe8136699f0456ffd896
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2023-01-21 10:39:48 +0200

    init order source attribution as soon as consent type defined.

--- assets/js/wp-consent-api-integration.js
+++ assets/js/wp-consent-api-integration.js
@@ -1,18 +1,29 @@
 ( function ( $, data ) {
     'use strict';
 
-    // check if window.consent_api is available
+    // check init order source attribution on consent change
     window.allowTracking = false;
+    const consentCategory = 'marketing';
     document.addEventListener("wp_listen_for_consent_change", function (e) {
         var changedConsentCategory = e.detail;
         for (var key in changedConsentCategory) {
             if (changedConsentCategory.hasOwnProperty(key)) {
-                if (key === 'marketing' && changedConsentCategory[key] === 'allow') {
+                if (key === consentCategory && changedConsentCategory[key] === 'allow') {
                     allowTracking = true;
                     woocommerce_order_source_attribution.initOrderTracking();
                 }
             }
         }
     });
+
+    // init order source attribution as soon as consent type defined
+    $(document).on("wp_consent_type_defined", activateOrderTrackingCookies);
+
+    function activateOrderTrackingCookies(consentData) {
+        if (wp_has_consent(consentCategory)) {
+            allowTracking = true;
+            woocommerce_order_source_attribution.initOrderTracking();
+        }
+    }
 
 } )( jQuery );

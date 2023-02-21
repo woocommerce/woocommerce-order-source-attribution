292a306f289504d52aa49081bee4ebe739ddfae8
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2023-02-21 19:47:41 +0200

    Add removing dropped cookies when consent change.

--- assets/js/wccom-integration.js
+++ assets/js/wccom-integration.js
@@ -1,10 +1,12 @@
 window.addEventListener( 'load' , function ( e ) {
 	if ( window.wccom && window.wccom.canTrackUser('analytics') ) {
+
 		if ( ! wccom || ! wccom.canTrackUser( 'analytics' ) ) {
 			return;
 		}
 		window.woocommerce_order_source_attribution.setAllowTrackingConsent( true );
 	}
+
 } );
 
 

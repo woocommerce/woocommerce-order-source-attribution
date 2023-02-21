15cac98b38d7a71aac4ad2fbc7c79a76feb10589
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2023-02-21 18:12:52 +0200

    simplify client side logic.

--- assets/js/wccom-integration.js
+++ assets/js/wccom-integration.js
@@ -1,14 +1,10 @@
 window.addEventListener( 'load' , function ( e ) {
-	function activateOrderTrackingCookies() {
-		if ( window.wccom && window.wccom.canTrackUser('analytics') ) {
-			if ( ! wccom || ! wccom.canTrackUser( 'analytics' ) ) {
-				return;
-			}
-			window.woocommerce_order_source_attribution.setAllowTrackingConsent( true );
+	if ( window.wccom && window.wccom.canTrackUser('analytics') ) {
+		if ( ! wccom || ! wccom.canTrackUser( 'analytics' ) ) {
+			return;
 		}
+		window.woocommerce_order_source_attribution.setAllowTrackingConsent( true );
 	}
-
-	activateOrderTrackingCookies();
 } );
 
 

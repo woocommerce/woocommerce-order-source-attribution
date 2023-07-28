9248ad73ea9411dcf835dda60ea19e484075e80c
Jeremy Pry <jeremy.pry@gmail.com>
2023-07-28 15:04:26 -0400

    Initial file for the customer history data

---
+++ templates/customer-history.php
@@ -0,0 +1,23 @@
+<?php
+declare( strict_types=1 );
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Variables used in this file.
+ *
+ *
+ */
+
+?>
+
+<div class="customer-history order-source-attribution-metabox">
+	<h4><?php esc_html_e( 'Total orders', 'woocommerce-order-source-attribution' ); ?></h4>
+
+
+	<h4><?php esc_html_e( 'Total spend', 'woocommerce-order-source-attribution' ); ?></h4>
+
+
+	<h4><?php esc_html_e( 'Average order value', 'woocommerce-order-source-attribution' ); ?></h4>
+
+</div>
